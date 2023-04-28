c3d649b305342cc7a88d2ef9da575afe2e98faa5
strelnikov <for.back.school@yandex.ru>
2023-04-28 23:49:39 +0300

    Create friendly messages

--- src/main/java/com/example/telegram/model/constant/DefaultMessages.java
+++ src/main/java/com/example/telegram/model/constant/DefaultMessages.java
@@ -3,8 +3,9 @@ package com.example.telegram.model.constant;
 public class DefaultMessages {
     public static String IN_ACCOUNT = """
         Я пока не знаю такой команды. Пожалуйста, воспользуйся существующим функционалом.
+        
         Если ты хочешь чтобы мой функционал был шире - напиши моим создателям, вот их ссылки:
-        https://t.me/allokanic
-        https://t.me/eridiium
+        @allokanic
+        @eridiium
         """;
 }
